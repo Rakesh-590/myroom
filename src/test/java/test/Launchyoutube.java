a1bc7935a109515a9baa6469325f58b04e33d70b
Rakesh-590 <ranganath.a0193@gmail.com>
2021-03-20 15:16:22 +0530

    first commit

---
+++ src/test/java/test/Launchyoutube.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launchyoutube {
+
+	
+@Test
+	
+	public void launchyoutube() {
+		
+		/*
+		 * System.setProperty("Webdriver.chrome.driver",
+		 * "C:\\Users\\TNT\\Desktop\\chromedriver.exe");
+		 * 
+		 * WebDriver driver=new ChromeDriver();
+		 * 
+		 * driver.get("https://www.youtube.com/"); driver.manage().window().maximize();
+		 */
+	 System.out.println("Good Exceution");	
+	}
+
+}
