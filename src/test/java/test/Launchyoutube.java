cec784446f2defddbb103ceded53b18b7f8b8e8c
Rakesh-590 <ranganath.a0193@gmail.com>
2021-03-22 16:12:06 +0530

    Today's Changes

--- src/test/java/test/Launchyoutube.java
+++ src/test/java/test/Launchyoutube.java
@@ -7,19 +7,16 @@ import org.testng.annotations.Test;
 public class Launchyoutube {
 
 	
-@Test
+public static void main(String[] args) {
 	
-	public void launchyoutube() {
 		
 		
-		  System.setProperty("Webdriver.chrome.driver",
-		  "C:\\Users\\TNT\\Desktop\\chromedriver.exe");
-		  
-		  WebDriver driver=new ChromeDriver();
-		  
-		  driver.get("https://www.youtube.com/"); driver.manage().window().maximize();
-		 
-	 System.out.println("Good Exceution");	
+		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\TNT\\Desktop\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		
+		driver.manage().deleteAllCookies();
 	}
+
 
 }
